3bc78d02eec1531d5b5a6cb0baad9d98bb833aa0
Madi <mayradiazchambilla327@gmail.com>
2025-05-29 23:02:06 -0500

    Mejora en el ingreso de cantidad de numeros para que no reciba negativos

--- Numerosprimos.cpp
+++ Numerosprimos.cpp
@@ -5,12 +5,17 @@ int main(){
 	int C;
 	int M=0;
 	int i=1;
-	cout<<Cantidad de multiplos de 7 que desee es: <<endl;
+	cout<< "Cantidad de multiplos de 7 que desee es: "<<endl;
 	cin>> C;
+	if (C>=1){
+		cout << "Los numeros son: "<< endl;
 	while (i<=C){
 		M=M+7;
-		   cout << M << ;
+		   cout << M<< endl;
 		   i++;
 	}
+    }else {
+	cout<< "El numero debe ser positivo" <<endl;
+    }
 	return 0;
 }
