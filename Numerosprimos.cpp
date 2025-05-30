0b3e8da08b38e8cbcc12398ac5e52cb487e065d6
Madi <mayradiazchambilla327@gmail.com>
2025-05-29 22:16:22 -0500

    Agregando nuevo archivo en repetitivas

---
+++ Numerosprimos.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int C;
+	int M=0;
+	int i=1;
+	cout<<Cantidad de multiplos de 7 que desee es: <<endl;
+	cin>> C;
+	while (i<=C){
+		M=M+7;
+		   cout << M << ;
+		   i++;
+	}
+	return 0;
+}
