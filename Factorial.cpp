296b009f8140a57af41e6153ac1da9b8e523351e
Madi <mayradiazchambilla327@gmail.com>
2025-05-30 22:07:21 -0500

    Mejora en el ingreso del programa, e integrando un else despues de un if ,para que si el numero es negativo el usuario tenga la posibilidad de ingresar un numero por segunda vez

--- Factorial.cpp
+++ Factorial.cpp
@@ -3,13 +3,35 @@ using namespace std;
 
 int main (){
 	int N;
-	int f=1, i=1;
-	cout<<Ingrese un numero:  <<endl;
+	int f=1, i=1; 
+	bool repetir = true;
+	cout<<"BIENVENIDO"<<endl;
+	cout<<"Ingrese un numero: " <<endl;
 	cin>>N;
+	if (N>=1){
 	while (i<=N){
         f=f*i;
         i++;
 	}
-	cout<<El factorial del numero: <<f<<endl;	
+}
+else{ 
+cout << "Vuelve a intentarlo (S/N): ";
+        char respuesta;
+        cin >> respuesta;
+
+        if (respuesta == 'N' || respuesta == 'n') {
+            repetir = false;
+        }
+        cout<<"Ingrese un numero: " <<endl;
+	cin>>N;
+	if (N>=1){
+	while (i<=N){
+        f=f*i;
+        i++;
+	}
+}
+    }
+	cout<<"El factorial del numero: "<< N << " es " <<endl;
+	cout<<f<<endl;
 	return 0;
 }
