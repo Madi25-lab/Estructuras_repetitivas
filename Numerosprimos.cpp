0ce03ebb0344061a8d7be72c91c539b5b9a7bb81
Madi <mayradiazchambilla327@gmail.com>
2025-05-31 21:32:41 -0500

    Mejora en la integracion de una funcion en el proceso de resolucion

--- Numerosprimos.cpp
+++ Numerosprimos.cpp
@@ -5,21 +5,19 @@ int main(){
 	int C;
 	int M=0;
 	int i=1;
-	cout << "¡Bienvenido a mi programa!" << endl;
-    cout << "Estoy listo para ayudarte " << endl;
 	cout<< "Cantidad de multiplos de 7 que desee es: "<<endl;
 	cin>> C;
 	if (C>=1){
-		cout << "Los numeros son: "<< endl;
-	while (i<=C){
-		M=M+7;
-		   cout <<" - " <<M <<endl;
+		M=M+7;      
+		   cout <<" Los numeros son "<<endl;
 		   i++;
-	}
-    }else {
+	}                   
+    }else {          
 	cout<< "El numero debe ser positivo" <<endl;
     }
     
     cout << "¡Gracias por utilizar mi programa!" << endl;
 	return 0;
 }
+              
+
--- Suma.cpp
+++ Suma.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int C;
-	int S=0;
-	int i=1;
-	cout<<"La cantidad de numeros es: ";
-	cin>> C;
-	if (C>=1){
+int suma(int C) {
+int i=1;
+int S=0;
+if (C>=1){
 	while (i<=C){
 		S=S+i;
 		i=i+1;
 	}
 }
+return S;
+}
+
+
+
+int main(){
+	int C, S;
+	cout<<"La cantidad de numeros es: ";
+	cin>> C;
+	S=suma(C);
 	cout<< "La suma es: "<< S <<endl;
 	return 0;
 }
