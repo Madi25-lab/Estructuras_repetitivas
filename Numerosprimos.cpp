2fcd237572352d90fc1a82d1623e27d75aceef25
Madi <mayradiazchambilla327@gmail.com>
2025-06-01 21:56:40 -0500

    Mejora en la implemetacion de un for int en el programa integrando de tal manera al 0 como multiplo inicial

--- Numerosprimos.cpp
+++ Numerosprimos.cpp
@@ -4,20 +4,20 @@ using namespace std;
 int main(){
 	int C;
 	int M=0;
-	int i=1;
+	cout << "Bienvenido a mi programa" << endl;
 	cout<< "Cantidad de multiplos de 7 que desee es: "<<endl;
 	cin>> C;
+	
 	if (C>=1){
-		M=M+7;      
-		   cout <<" Los numeros son "<<endl;
-		   i++;
-	}                   
-    }else {          
+		cout <<"Los numeros son "<<endl;
+		for (int i=0; i<=C-1; i++){
+     	M=7*i; 
+		cout << M <<endl;
+		}
+		}else {          
 	cout<< "El numero debe ser positivo" <<endl;
     }
     
     cout << "¡Gracias por utilizar mi programa!" << endl;
 	return 0;
 }
-              
-
