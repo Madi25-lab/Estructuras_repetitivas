3cafc659f42a90a475d45c89d6257732b9d9c48c
Madi <mayradiazchambilla327@gmail.com>
2025-06-02 08:07:51 -0500

    Mejora en la sustitucion de un while por un for y dando un saluda antes y despues de comenzar su ejecucion del problema

--- Suma.cpp
+++ Suma.cpp
@@ -5,21 +5,21 @@ int suma(int C) {
 int i=1;
 int S=0;
 if (C>=1){
-	while (i<=C){
-		S=S+i;
-		i=i+1;
+	for (int i=1;i<=C;i++){
+	S=S+i;	
 	}
 }
 return S;
 }
 
 
-
 int main(){
 	int C, S;
+	cout<<"Bienvenido a mi programa "<<endl;
 	cout<<"La cantidad de numeros es: ";
 	cin>> C;
 	S=suma(C);
 	cout<< "La suma es: "<< S <<endl;
+	cout<<"Gracias ";
 	return 0;
 }
